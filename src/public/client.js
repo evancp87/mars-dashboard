c7297f8e9f96744b029d637f2921ca861c4b4d84
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-03-27 18:22:57 +0100

    removed another localhost from fetch call

--- src/public/client.js
+++ src/public/client.js
@@ -249,7 +249,7 @@ const ImageOfTheDay = (apod) => {
 const getImageOfTheDay = (state) => {
   let { apod } = state;
 
-  fetch("http://localhost:3000/apod")
+  fetch("/apod")
     .then((res) => res.json())
     .then((apod) => updateStore(store, { apod }));
 };
